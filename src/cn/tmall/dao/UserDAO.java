65a7d680dbc32e180bf460cf5d8b3e0b996f9c21
zhyyn <247069532@qq.com>
2017-06-13 10:27:33 +0800

    111

--- src/cn/tmall/dao/UserDAO.java
+++ src/cn/tmall/dao/UserDAO.java
@@ -1,7 +1,24 @@
 package cn.tmall.dao;
 
+import cn.tmall.util.DBUtil;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class UserDAO {
     public int total(){
-        return 0;
+        int total=0;
+        try (Connection c= DBUtil.getConnection(); Statement s=c.createStatement()){
+            String sql="select count(*) from user";
+            ResultSet rs=s.executeQuery(sql);
+            if (rs.next()){
+                total=rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return total;
     }
 }
